38fb1742cd1e90b2ccbe4d20878df8964e6c170f
KaralinskiDavid <64661403+KaralinskiDavid@users.noreply.github.com>
2020-09-12 23:36:37 +0300

    Add files via upload
    Добавлена консольная часть
    Не решена проблема неизменяемости TraceResult для пользователя

---
+++ ConsoleApp9/ConsoleApp9/JsonSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+using TracerLib;
+
+namespace ConsoleApp9
+{
+    class JsonSerializator : Serializator
+    {
+
+        public JsonSerializator()
+        {
+        }
+
+        JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.None,
+            NullValueHandling = NullValueHandling.Ignore,
+            Formatting = Formatting.Indented,
+        };
+
+        public override string Serialize(TraceResult tr)
+        {
+            string JsonString = JsonConvert.SerializeObject(tr, JsonSettings);
+            //using (StreamWriter fStream = new StreamWriter(FilePath))
+            //{
+            //    fStream.Write(JsonString);
+            //}
+            return JsonString;
+        }
+    }
+}
